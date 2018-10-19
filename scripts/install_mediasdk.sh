8830606279a1640f4ac84f09a0d85353ef562d34
Alexei Zavjalov <alexei.zavjalov@intel.com>
2018-10-19 08:14:13 -0700

    Clone only one branch for MSDK
    Signed-off-by: Alexei Zavjalov <alexei.zavjalov@intel.com>

--- scripts/install_mediasdk.sh
+++ scripts/install_mediasdk.sh
@@ -3,9 +3,8 @@
 . ../scripts/set_environment.sh
 
 rm -rf msdk
-git clone https://github.com/Intel-Media-SDK/MediaSDK msdk
+git clone -b mediasdk_for_cv --single-branch https://github.com/Intel-Media-SDK/MediaSDK msdk
 cd msdk
-git checkout origin/mediasdk_for_cv
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/intel/ ..
