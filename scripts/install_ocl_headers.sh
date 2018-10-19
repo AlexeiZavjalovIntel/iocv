cbd837cf596bb0725a05240f6355e65f53abd778
Alexei Zavjalov <alexei.zavjalov@intel.com>
2018-10-19 08:23:52 -0700

    Fix small bug in Khronos headers script
    Signed-off-by: Alexei Zavjalov <alexei.zavjalov@intel.com>

--- scripts/install_ocl_headers.sh
+++ scripts/install_ocl_headers.sh
@@ -6,4 +6,4 @@ rm -rf ocl_headers
 git clone https://github.com/KhronosGroup/OpenCL-Headers.git ocl_headers
 cd ocl_headers/
 sudo cp -rf CL/ /opt/intel/include/
-sudo ln -s /opt/intel/include/CL/cl_va_api_media_sharing_intel.h /opt/intel/include/CL/va_ext.h
+sudo ln -sf /opt/intel/include/CL/cl_va_api_media_sharing_intel.h /opt/intel/include/CL/va_ext.h
