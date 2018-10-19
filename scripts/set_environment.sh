f4563538ba45b811752f6e7bdd5d9bef0e9938b7
Alexei Zavjalov <alexei.zavjalov@intel.com>
2018-10-19 08:04:27 -0700

    Changed proxy settings
    Signed-off-by: Alexei Zavjalov <alexei.zavjalov@intel.com>

--- scripts/set_environment.sh
+++ scripts/set_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export http_proxy="http://proxy.sc.intel.com:911"
-export https_proxy="https://proxy.sc.intel.com:911"
+export http_proxy="http://proxy-chain.intel.com:911"
+export https_proxy="https://proxy-chain.intel.com:911"
 
 export LD_LIBRARY_PATH="/opt/intel/lib/"
 export PKG_CONFIG_PATH="/opt/intel/lib/pkgconfig"
