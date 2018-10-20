47a6dba0276b551eaafbc6d84cf56905277d746b
Alexei Zavjalov <alexei.zavjalov@intel.com>
2018-10-20 16:27:46 -0700

    Git-lfs workaround for MSDK
    Signed-off-by: Alexei Zavjalov <alexei.zavjalov@intel.com>

--- scripts/install_mediasdk.sh
+++ scripts/install_mediasdk.sh
@@ -5,7 +5,10 @@ set -e
 . ../scripts/set_environment.sh
 
 rm -rf msdk
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo -E bash
+sudo apt-get update -y
 sudo apt-get install git-lfs -y
+git lfs install
 GIT_LFS_SKIP_SMUDGE=1 git clone -b mediasdk_for_cv --single-branch https://github.com/Intel-Media-SDK/MediaSDK msdk
 cd msdk
 mkdir build
